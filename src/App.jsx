ebc40aa6ed22ae094dd707bb33fdc2d2288e8812
Jan Gross <jan.gross.cze@gmail.com>
2023-10-13 12:23:30 +0200

    basic components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 
 import './App.css'
 
+import { useState } from 'react';
+
+const url = "https://course-api.com/react-tours-project";
+
 function App() {
-  
+  const [loading, setLoading] = useState(true)
 
   return (
     <>
-    
+      
        
     </>
   )
---
+++ src/components/OneTour.jsx
@@ -0,0 +1,6 @@
+
+export const OneTour = () => {
+  return (
+    <div>OneTour</div>
+  )
+}
